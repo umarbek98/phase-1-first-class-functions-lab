96fddb0849ecad7973a959e13883069b6f00700d
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-01-11 12:31:25 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (function () {
+    return ['Antonia', 'Nuru'];
+});
+
+const returnLastTwoDrivers = (function () {
+    return ['Amari', 'Mo'];
+});
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return(function (fare){
+        return num * fare
+    })
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(selectingDrivers, returnLastTwoDrivers) {
+   switch(selectingDrivers, returnLastTwoDrivers) {
+    case "Antonia":
+        return returnFirstTwoDrivers();
+    case "Nuru":
+        return returnFirstTwoDrivers();
+    case returnLastTwoDrivers:
+        return returnLastTwoDrivers();
+   }
+}
+
+console.log(selectDifferentDrivers(returnLastTwoDrivers))
